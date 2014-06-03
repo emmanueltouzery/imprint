877ad3e2c4c464691016805131316bbbcb69fdd2
Emmanuel Touzery <etouzery@gmail.com>
2014-06-03 21:53:35 +0200

    displaying a preview of the selected text style in the main settings window

--- SettingsWindow.hs
+++ SettingsWindow.hs
@@ -5,11 +5,16 @@ import Graphics.UI.Gtk hiding (styleSet)
 import Graphics.Rendering.Cairo hiding (width, height, x)
 import Data.IORef
 import Data.AppSettings (getSetting', Conf, setSetting)
+import Data.List
+import Data.Maybe (fromJust)
 
 import TextStylesSettings
+import Settings
 
 showSettingsWindow :: Builder -> IORef Conf -> IO ()
 showSettingsWindow builder latestConfig = do
+	ctxt <- cairoCreateContext Nothing -- TODO creating cairo ctxt all over the place...
+
 	settingsWindow <- builderGetObject builder castToWindow "settings_window"
 	locationPicker <- builderGetObject builder castToDrawingArea "locationpicker"
 	widgetSetSizeRequest locationPicker 150 500
@@ -18,6 +23,18 @@ showSettingsWindow builder latestConfig = do
 	pickTextStyleBtn <- builderGetObject builder castToButton "picktextstylebtn"
 	pickTextStyleBtn `on` buttonActivated $ do
 		showTextStyleListDialog builder latestConfig settingsWindow
+
+	text <- layoutEmpty ctxt
+	text `layoutSetText` "2014-04-01"
+	textStylePreview <- builderGetObject builder castToDrawingArea "textstylepreview"
+	textStylePreview `on` draw $ do
+		conf <- liftIO $ readIORef latestConfig
+		let cTextStyle = fromJust $ find ((== getSetting' conf selectedTextStyleId) . styleId)
+			$ getSetting' conf textStyles
+		liftIO $ do
+			updateFontFromTextStyle ctxt cTextStyle
+			setFontSizeForWidget ctxt text textStylePreview
+		renderText text cTextStyle
 
 	windowSetDefaultSize settingsWindow 600 500
 	--prepareTextStylePreview builder latestConfig
