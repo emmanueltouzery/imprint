be49a46a9bb3dc2221bc274b8444113a0906be45
Emmanuel Touzery <etouzery@gmail.com>
2014-06-04 21:30:26 +0200

    slow progress towards preview on the left pane

--- SettingsWindow.hs
+++ SettingsWindow.hs
@@ -7,6 +7,7 @@ import Data.IORef
 import Data.AppSettings (getSetting', Conf, setSetting)
 import Data.List
 import Data.Maybe (fromJust)
+import Control.Monad (liftM)
 
 import TextStylesSettings
 import Settings
@@ -16,9 +17,14 @@ showSettingsWindow builder latestConfig = do
 	ctxt <- cairoCreateContext Nothing -- TODO creating cairo ctxt all over the place...
 
 	settingsWindow <- builderGetObject builder castToWindow "settings_window"
-	locationPicker <- builderGetObject builder castToDrawingArea "locationpicker"
-	widgetSetSizeRequest locationPicker 150 500
-	locationPicker `on` draw $ drawCurLocationConfig
+	imageLayout <- builderGetObject builder castToDrawingArea "image_layout"
+	
+	aspectRatioCombo <- builderGetObject builder castToComboBox "aspect_ratio_combo"
+	aspectRatioCombo `on` changed $ widgetQueueDraw imageLayout
+
+	textLayout <- layoutEmpty ctxt
+	textLayout `layoutSetText` "2014-04-01"
+	imageLayout `on` draw $ drawImageLayout imageLayout aspectRatioCombo latestConfig ctxt textLayout
 
 	pickTextStyleBtn <- builderGetObject builder castToButton "picktextstylebtn"
 	pickTextStyleBtn `on` buttonActivated $ do
@@ -40,6 +46,39 @@ showSettingsWindow builder latestConfig = do
 	--prepareTextStylePreview builder latestConfig
 	widgetShowAll settingsWindow
 
-drawCurLocationConfig :: Render ()
-drawCurLocationConfig = return ()
+data AspectRatio = FourThree
+	| ThreeTwo
+
+getHeightMultiplier :: ComboBox -> IO Double
+getHeightMultiplier aspectRatioCombo = do
+	v <- comboBoxGetActive aspectRatioCombo
+	return $ case v of
+		0 -> 3/4
+		_ -> 2/3
+
+drawImageLayout :: DrawingArea -> ComboBox -> IORef Conf -> PangoContext -> PangoLayout -> Render ()
+drawImageLayout drawingArea aspectRatioCombo latestConfig ctxt text = do
+	heightMultiplier <- liftIO $ getHeightMultiplier aspectRatioCombo
+	conf <- liftIO $ readIORef latestConfig
+
+	-- draw image borders...
+	w <- liftIO $ liftM fromIntegral $ widgetGetAllocatedWidth drawingArea
+	h <- liftIO $ liftM fromIntegral $ widgetGetAllocatedHeight drawingArea
+
+	let (effectiveW, effectiveH) = if w*heightMultiplier < h
+		then (w, w*heightMultiplier)
+		else (h/heightMultiplier, h)
+
+	rectangle 0 ((h-effectiveH)/2) effectiveW effectiveH
+	moveTo 0 0
+
+
+	-- bottom right corner
+	let cTextStyle = fromJust $ find ((== getSetting' conf selectedTextStyleId) . styleId)
+		$ getSetting' conf textStyles
+	liftIO $ do
+		updateFontFromTextStyle ctxt cTextStyle
+		setFontSizeForWidget ctxt text drawingArea
+	renderText text cTextStyle
+	return ()
 
