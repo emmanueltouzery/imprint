9bcbceddece8caa93b68166dc8ed97bc5fb2c91a
Emmanuel Touzery <etouzery@gmail.com>
2014-06-29 13:29:00 +0200

    desktop file for linux

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE CPP #-}
 
+import System.Process (rawSystem)
+
 #if CABAL_OS_WINDOWS
 
 import Distribution.Simple
@@ -8,6 +10,13 @@ main = defaultMain
 #else
 
 import Distribution.Simple.I18N.GetText
-main = gettextDefaultMain
+main = do
+	-- manage translations
+	gettextDefaultMain
+	-- install desktop file and icon
+	rawSystem "xdg-icon-resource" ["install", "--size", "128", "imprint-128.png", "graphics-imprint"]
+	rawSystem "xdg-desktop-menu" ["install", "graphics-imprint.desktop"]
+	return ()
+	
 
 #endif
